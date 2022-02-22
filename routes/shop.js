0b02cc9e0270d5744f509cf94a6bb650a1dc94b0
Juan Carlos Ramirez <jccortavee@gmail.com>
2022-02-22 14:29:08 -0600

    Implement MVC using express

--- routes/shop.js
+++ routes/shop.js
@@ -2,14 +2,10 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-const rootDir = require('../utils/path');
-const adminData = require('./admin');
+const productsController = require('../controllers/products');
 
-router.get('/', (req, res, next) => {
-    const products = adminData.products;
-    // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-    res.render('shop', { products, path: '/', pageTitle: 'Shop', hasProducts: products.length > 0,
-        activeShop: true, productCSS: true });
-});
+const rootDir = require('../utils/path');
+
+router.get('/', productsController.getProducts);
 
 module.exports = router;
